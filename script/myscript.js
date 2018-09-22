d37ac552b1baed7ec086d5b53159a668adc10091
PiiterLiao <pittpeterliao@gmail.com>
2018-09-22 10:03:04 +0800

    exchange floating words png, tune the top and 1680px top

--- script/myscript.js
+++ script/myscript.js
@@ -26,7 +26,29 @@ $(function(){
     $('.video-opening img').parallax('50%',0.3);
     $('#opening-word').parallax('50%',0.1);
 
-    // if ($(window).scrollTop()==700) {
-    //     $('#opening-word').css('opacity',0)
-    // }
 })
+
+window.sr = ScrollReveal({ 
+    // 參數設定[註1]
+    origin: "bottom",  // 起始位置
+    distance: "80px",  // 距離
+    duration: 1000,  // 動畫時間
+    delay: 200,  // 動畫延遲時間
+    rotate: { x: 0, y: 0, z: 0 },  // 旋轉角度
+    opacity: 0,  // 透明度
+    scale: 1, // 縮放比例
+    easing: "cubic-bezier(0.6, 0.2, 0.1, 1)", // 動畫速度曲線
+    container: window.document.documentElement, // 外層
+    mobile: true, // 支援行動裝置
+    reset: false, // 每次都啟動動畫
+    useDelay: "always", // 延遲動畫次數
+    viewFactor: 0.2, // 當該物件在可視範圍內，則顯示此物件(0.2表示可視範圍20%)
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 }, // 當外層有設定間隔造成偏移，則請設定在此維持精準度
+    beforeReveal: function (domEl) { console.log(1) }, // 當啟動前，則執行此函式
+    beforeReset: function (domEl) {console.log(2) }, // 當重啟前，則執行此函式
+    afterReveal: function (domEl) {console.log(3) }, // 當啟動後，則執行此函式
+    afterReset: function (domEl) {console.log(4) } // 當重啟後，則執行此函式
+});
+sr.reveal( ".appear", {
+    // 參數設定[註1]
+});
