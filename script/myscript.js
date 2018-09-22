73c4008f94a76c971676d3c5ead0e77549ab3c31
PiiterLiao <pittpeterliao@gmail.com>
2018-09-22 11:32:09 +0800

    change to 1680, block-top mouseover disappear

--- script/myscript.js
+++ script/myscript.js
@@ -1,31 +1,21 @@
 $(window).scroll(function(){
-    if($(window).scrollTop()>80){
+    if($(window).scrollTop()>90){
         $('.ingredient img').animate({opacity: 0},700)
     }
-
     if ($(window).scrollTop()>900) {
         $('#opening-word').css({animation: 'fInOut 2.5s 0s 1 both'})
     }
-
-    // switch ($(window).scrollTop()) {
-    //     case 80:
-    //         $('.large-img img:nth-child(1)').animate({opacity: 0},700)
-    //         
-
-    //     case 700:
-    //         $('#opening-word').css('opacity',0)
-    //         break;
-            
-    //     default:
-    //         break;
-    // }
 })
 
 $(function(){
     $('.block-top').parallax('50%',0.8);
-    $('.video-opening img').parallax('50%',0.3);
+    $('.video-opening img').parallax('50%',0.4);
     $('#opening-word').parallax('50%',0.1);
 
+    $('.block-top').mouseover(function () { 
+        $('.floating-word').animate({opacity: 0},700)
+        $('.dental-text').animate({opacity: 1},700)
+    });
 })
 
 window.sr = ScrollReveal({ 
