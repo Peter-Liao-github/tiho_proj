6f9ad753279ded3b4bc253a7e4b48f320c4e1ec9
PiiterLiao <pittpeterliao@gmail.com>
2018-10-11 19:19:07 +0800

    ver. tc

---
+++ script/myscriptTc.js
@@ -0,0 +1,30 @@
+$(function(){
+    //關於區圖文切換
+    var aboutImgOrigin = ['img/TIHO_Web_Ingredient-22.png','img/TIHO_Web_Ingredient-23.png','img/TIHO_Web_Ingredient-24.png','img/TIHO_Web_Ingredient-25.png']
+    var aboutImg = ['img/TIHO_Web_Ingredient-26.png','img/TIHO_Web_Ingredient-27.png','img/TIHO_Web_Ingredient-28.png','img/TIHO_Web_Ingredient-29.png']
+    var aboutTextTitle = ['平價植牙','一條龍品質把關','全省服務','材料終身保固']
+    var aboutTextP = ['引進加密貨幣，使得人人都可以是消費者，也能成為投資者，享到低成本的植牙價格。','從日本品牌上游介入到診所醫療技術和牙療服務，再到假牙製作產業，能夠一條龍把關超越業界。','套件工具齊全，病患不會變成植牙孤兒。','不會在同病患身上收取兩次材料費用。']
+    
+    for (let i = 1; i < aboutImg.length+1; i++) {
+        $('img.about-'+i).click(function () {
+            hoverChange(i,4)
+        })
+    }
+
+    function hoverChange(i,j) {
+        for (let k = 1; k <= j; k++) {
+            $('img.about-'+k).attr('src',aboutImgOrigin[k-1])
+        }
+        
+        $('img.about-'+i).attr('src',aboutImg[i-1])
+        hoverShow($('img.about-'+i))
+        $('.about-text>h3>span').text(aboutTextTitle[i-1])
+        hoverShow($('.about-text>h3>span'))
+        $('.about-text>p').text(aboutTextP[i-1])
+        hoverShow($('.about-text>p'))
+    }
+    function hoverShow(e) {
+        e.css('opacity',0)
+            .animate({opacity: 1},500)
+    }
+})
